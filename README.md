93dee08f7b00673123bc5229e33373d0e54c1669
Caitlyn Mitchell <kikirafiki@gmail.com>
2022-02-10 19:52:09 -0800

    first function added.

---
+++ README.md
@@ -0,0 +1,5 @@
+Describe: numbers()
+Test:"It will return a list of numbers from 0-9. Numbers that contain the number 1 will be replaced with the string 'Beep!'"
+Code: numbers = 14;numbers(number);
+Epected Output:'"0","Beep!","2","3","4","5","6","7","8","9,"'
+
---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+//Business Logic
+
+$(document).ready(function() {
+  $("#mrRoboger").submit(function(event) {
+    event.preventDefault();
+    let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    const beep = ["Beep"];
+    
+    numbers.forEach(function(value, index) {
+    alert(value);
+  }:
+}):
