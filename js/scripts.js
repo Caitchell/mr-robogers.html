348260dccec89db35124d1f4b3a5417b1f502adb
Caitlyn Mitchell <kikirafiki@gmail.com>
2022-02-15 18:56:22 -0800

    fixed business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 //Business Logic
 
-numbersRoboger = function translator(number) {
-  let numbersRoboger = [0-15]
+function translator(number) {
+  let numbersRoboger = [];
 
   for (let index = 0; index <= number; index++) {
     if (index.toString().includes("3")) {
-      translation.push("Won't you be my neighbor?")
+      numbersRoboger.push("Won't you be my neighbor?")
     } else if (index.toString().includes("2")) {
       numbersRoboger.push("Boop!")
     } else if (index.toString().includes("1")) {
-      translation.push("Beep!");
+      numbersRoboger.push("Beep!");
     } else {
       numbersRoboger.push(index)
     }
